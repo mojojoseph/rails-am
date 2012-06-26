d8febb0e099b691c20c1167d9db238cea8ffa82f
Joseph Bell <jbell@numerex.com>
2012-06-26 10:54:07 -0500

    Simplification

--- app/processors/events_processor.rb
+++ app/processors/events_processor.rb
@@ -1,36 +1,9 @@
 class EventsProcessor < ApplicationProcessor
 
-  @@batchMax   = 5
-  @@batchQueue = []
-  @@maxQueueLifetime = 30 # seconds
-  @@queueBirthdate   = nil
-  
-  subscribes_to :events, {:ack => 'client'}
+  subscribes_to :events
 
   def on_message(message)
-    if @@batchQueue.empty? then
-      @@queueBirthdate = Time.now
-    end
-
-    queueLifetime = Time.now - @@queueBirthdate
-
-    if @@batchQueue.length >= @@batchMax or queueLifetime >= @@maxQueueLifetime then
-      flush_queue
-    else
-      queue_message message
-    end
-  end
-
-  def queue_message(message)
-    logger.debug "Queueing message:  #{message}"
-    @@batchQueue << message
-    puts "length = #{@@batchQueue.length}"
-  end
-
-  def flush_queue
-    logger.debug "Flush queue:"
-    @@batchQueue.each {|msg| puts "Flushing #{msg}"}
-    @@batchQueue.clear
+    puts "Message! #{message}"
   end
 
 end
--- config/messaging.rb
+++ config/messaging.rb
@@ -3,10 +3,7 @@
 # can also be used to configure filters, and processor groups
 #
 ActiveMessaging::Gateway.define do |s|
-  #s.destination :orders, '/queue/Orders'
-  #s.filter :some_filter, :only=>:orders
-  #s.processor_group :group1, :order_processor
   
-  s.destination :events, '/queue/gps.events'
+  s.destination :events, '/queue/inbound'
   
 end
